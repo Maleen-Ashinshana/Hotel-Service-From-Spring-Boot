3e72ad6a9951ec3eb07dc5ad134e30f281db3046
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-10-12 23:57:41 +0530

    first commit

---
+++ src/main/java/lk/ijse/gdse/dto/HotelImageDTO.java
@@ -0,0 +1,24 @@
+package lk.ijse.gdse.dto;
+
+import lk.ijse.gdse.entity.HotelEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class HotelImageDTO {
+    private int image_id;
+    private String front_image;
+    private String back_image;
+    private String inside_image;
+    private HotelEntity hotelEntity;
+
+    public HotelImageDTO(int image_id, String front_image, String back_image, String inside_image) {
+        this.image_id = image_id;
+        this.front_image = front_image;
+        this.back_image = back_image;
+        this.inside_image = inside_image;
+    }
+}
---
+++ src/main/java/lk/ijse/gdse/entity/HotelImageEntity.java
@@ -0,0 +1,38 @@
+package lk.ijse.gdse.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Table(name = "hotelImage")
+public class HotelImageEntity implements SuperEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int image_id;
+    @Column(nullable = false )
+    @Lob
+    private String front_image;
+    @Column(nullable = false )
+    @Lob
+    private String back_image;
+    @Column(nullable = false )
+    @Lob
+    private String inside_image;
+    @ManyToOne
+    @JoinColumn(name = "hotel_id",referencedColumnName = "hotel_id")
+    private HotelEntity hotelEntity;
+
+    public HotelImageEntity(int image_id, String front_image, String back_image, String inside_image) {
+        this.image_id = image_id;
+        this.front_image = front_image;
+        this.back_image = back_image;
+        this.inside_image = inside_image;
+    }
+}
